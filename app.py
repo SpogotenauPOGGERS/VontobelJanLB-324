fe0f698bc0c273e0512970f49ba79d84c77b863e
J.Vontobel.inf21 <j.vontobel.inf21@stud.bbbaden.ch>
2023-09-28 18:01:17 +0200

    happiness feature added

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ entries = []
 @dataclass
 class Entry:
     content: str
+    happiness: str = ""
     timestamp: datetime = datetime.now()
 
 
@@ -48,8 +49,9 @@ def logout():
 @app.route("/add_entry", methods=["POST"])
 def add_entry():
     content = request.form.get("content")
-    if content:
-        entry = Entry(content=content)
+    happiness = request.form.get("happiness")
+    if content and happiness:
+        entry = Entry(content=content, happiness=happiness)
         entries.append(entry)
     return redirect(url_for("index"))
 
